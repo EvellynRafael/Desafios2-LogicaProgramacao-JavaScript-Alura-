07f9a23923464d26193443d8dde364de7dc450d2
Evellyn Rafael <71461331+EvellynRafael@users.noreply.github.com>
2025-02-17 12:11:58 -0300

    Create listas.js
    Primeiros desafios da etapa Listas

---
+++ listas.js
@@ -0,0 +1,19 @@
+  // DESAFIO LISTAS
+  //lista generica
+  let listaGenerica = [];
+
+  //linguagens
+  let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+  //adicionar linguagens
+  let adicionarLinguagem = linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+  console.log(linguagensDeProgramacao);
+  
+  //Exibir o primeiro elemento da lista
+  let nomes = ['Maria', 'João', 'Paulo'];
+  console.log(nomes[0]);
+
+  //Exibir segundo elemento
+  console.log(nomes[1]);
+
+//Exibir terceiro elemento
+  console.log(nomes[2]);
